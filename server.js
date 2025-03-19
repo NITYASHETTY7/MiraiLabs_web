bd3397deedba1a6723587037bbf94f2cd8899db2
tariq <tariqiqbal071@gmail.com>
2025-03-19 19:44:39 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = 3000;
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Catch all routes and redirect to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
